94c529e193986bc1fef380223da4ffecdfe88d1a
adityajha21087 <adityajha21087@gmail.com>
2026-08-24 08:27:39 +0530

    Time: 0 ms (100.00%) | Memory: 34.4 MB (16.14%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -11,33 +11,65 @@
  */
 class Solution {
 public:
-   TreeNode* iop(TreeNode* root){
-    TreeNode*pre=root->left;
-    while(pre->right!=NULL){
-        pre=pre->right;
+    TreeNode* precessor(TreeNode*pre){
+      
+        pre=pre->left;
+        while(pre->right){
+            pre=pre->right;
+        }
+        return pre;
     }
-    return pre;
-   }
     TreeNode* deleteNode(TreeNode* root, int key) {
       if(root==NULL)   return NULL;
-      if(root->val==key){
-        // key is a leaf Node
-        if(root->left==NULL && root->right==NULL ) return NULL;
-        // key has one node
-        else if(root->left==NULL || root->right==NULL){
-            if(root->left !=NULL) return root->left;
-            else return root->right;
-        }
-        // key has two node
-        else {
-            // inorder preccesor
-            TreeNode*pre=iop(root);
-            root->val=pre->val;
-            root->left=deleteNode(root->left, pre->val);
-        }
+      if(root->val==key)
+    {  //node has no child
+      if(root->left==NULL && root->right==NULL) return NULL;
+      // node has only one child
+      if(root->left==NULL || root->right==NULL){
+        if(root->left) return root->left;
+        else return root->right;
       }
-      root->left=deleteNode( root->left,  key);
-      root->right=deleteNode(root->right, key);
-      return root;
+      // node has two child
+      else {
+        TreeNode*p=precessor(root);
+        root->val=p->val;
+       root->left= deleteNode(root->left,p->val);
+      }}
+    root->left=deleteNode(root->left,key);
+    root->right=deleteNode(root->right,key);
+    return root;
     }
 };
+
+// class Solution {
+// public:
+//    TreeNode* iop(TreeNode* root){
+//     TreeNode*pre=root->left;
+//     while(pre->right!=NULL){
+//         pre=pre->right;
+//     }
+//     return pre;
+//    }
+//     TreeNode* deleteNode(TreeNode* root, int key) {
+//       if(root==NULL)   return NULL;
+//       if(root->val==key){
+//         // key is a leaf Node
+//         if(root->left==NULL && root->right==NULL ) return NULL;
+//         // key has one node
+//         else if(root->left==NULL || root->right==NULL){
+//             if(root->left !=NULL) return root->left;
+//             else return root->right;
+//         }
+//         // key has two node
+//         else {
+//             // inorder preccesor
+//             TreeNode*pre=iop(root);
+//             root->val=pre->val;
+//             root->left=deleteNode(root->left, pre->val);
+//         }
+//       }
+//       root->left=deleteNode( root->left,  key);
+//       root->right=deleteNode(root->right, key);
+//       return root;
+//     }
+// };
