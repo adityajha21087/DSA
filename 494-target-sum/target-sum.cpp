9807f9bd24b2a6e62a63aad3360149e667eeeb2c
adityajha21087 <adityajha21087@gmail.com>
2026-09-15 16:55:00 +0530

    Time: 15 ms (50.97%) | Memory: 25.9 MB (25.66%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int f(int i, int sum, vector<int > &nums, int target){
-      if(i==nums.size()){
-        if(target==sum) return 1;
-        else return 0;
-      }
-    // if(dp[i][sum]!=-1) return dp[i][sum];
-    int usePlus=f(i+1, sum+nums[i], nums, target);
-    int useNeg=f(i+1, sum-nums[i], nums,  target);
-    return usePlus+useNeg;
+ 
+    int f(int i, int res, vector<int> &nums, vector<vector<int>> & dp, int target, int sum){
+        if(i==nums.size()){
+            if(res==target) return 1;
+            else return 0;
+        }
+       if (dp[i][res+sum]!=-1) return dp[i][res+sum];
+       int add=f(i+1, res+nums[i],nums, dp, target,sum);
+       int sub=f(i+1, res-nums[i], nums, dp, target,sum);
+       return dp[i][sum+res]=add+sub;
     }
+  
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        // vector<vector<int>> dp(n,vector<int> (target+1, -1));
-        return f(0,0,nums,  target);
+        int n =nums.size();
+        int sum=0;
+    for(auto ele: nums){
+        sum+=ele;
     }
-};
+    vector<vector<int>> dp(n, vector<int> (2*sum+1,-1));
+    return f(0, 0, nums, dp,  target,sum);
+    }
+};                                                                                                                                                
