46a44e1c0ae7f3393be63c6b4f404401cd828e3c
adityajha21087 <adityajha21087@gmail.com>
2026-09-18 11:40:41 +0530

    Time: 277 ms (12.41%) | Memory: 14.1 MB (24.55%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-bool isPerfect(int n){
+ bool isPerfect(int n){
     int sqr=sqrt(n);
     return sqr*sqr==n;
 }
+  int f(int n , vector<int> & dp){
+    if(isPerfect(n)) return 1;
+    int mini=n;
+    if(dp[n]!=-1) return dp[n];
+    for(int i=1; i*i<=n; i++){
+        int count=f(n-i*i,dp)+f(i*i,dp);
+        mini=min(count,mini);
+    }
+    return dp[n]=mini;
+  }
     int numSquares(int n) {
-      
-        vector<int> dp(n+1);
-
-        for(int i=1; i<=n; i++){
-            if(isPerfect(i)) dp[i]=1;
-            else{
-                  int mini=INT_MAX;
-                for(int j=1; j*j<=i; j++){
-                   int  count = dp[j*j]+dp[i-j*j];
-                   mini=min(mini,count);
-                }
-                dp[i]=mini;
-            }
-        }
-     return dp[n];
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
     }
 };
